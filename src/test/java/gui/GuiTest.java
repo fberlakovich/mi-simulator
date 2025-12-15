d3243779fbaae116bf244f1d6259975a0b176e9f
Felix Berlakovich <felix@berlakovich.at>
2025-12-15 14:51:59 +0100

    add GUI and regression testing

---
+++ src/test/java/gui/GuiTest.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * Marker interface for GUI tests.
+ * Used to categorize tests that require a display.
+ *
+ * <p>To exclude GUI tests from command line:</p>
+ * <pre>
+ * ./gradlew test -PskipGuiTests
+ * </pre>
+ */
+public interface GuiTest {
+}
